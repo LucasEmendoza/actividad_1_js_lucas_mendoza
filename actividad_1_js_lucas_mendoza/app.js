825f3a5727939f0171db8b404e98a72a8158c21d
LucasEmendoza <105325882+LucasEmendoza@users.noreply.github.com>
2022-07-18 21:10:13 -0500

    Add files via upload

---
+++ actividad_1_js_lucas_mendoza/app.js
@@ -0,0 +1,10 @@
+let name = 'Lucas',
+  lastname = "Mendoza",
+  yearofbirth = 1992,
+  cityofbirth = "Medellin";
+
+  console.log(
+    `Hola, mi nombre es ${name} ${lastname}, vivo en ${cityofbirth}
+   y tengo ${2022 - yearofbirth} años.`)
+
+  
